0d2cdd964495dffd44bfd0792fdd74ee48511402
YaronKoresh <aharonkoresh1@gmail.com>
2021-09-01 20:06:29 +0300

    Update index.tsx

--- Mobile/src/index.tsx
+++ Mobile/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -11,5 +10,4 @@ ReactDOM.render(
 	document.getElementById('root')
 );
 
-serviceWorkerRegistration.unregister();
 reportWebVitals();
