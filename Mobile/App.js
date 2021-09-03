a3db4fd24909269d2ba77891d821b775efdd1bde
YaronKoresh <aharonkoresh1@gmail.com>
2021-09-03 04:39:34 +0300

    Update App.js

--- Mobile/App.js
+++ Mobile/App.js
@@ -9,16 +9,18 @@ import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
 
 import './more/pages/home';
+import './more/pages/code';
+import './more/pages/guide';
+import './more/pages/about';
 
 export default function App() {
 	return (
 		<IonApp>
 			<IonReactRouter>
 				<IonRouterOutlet>
-					<Route path="/" exact={true}>
-						<Redirect to="/home" />
-					</Route>
+					<Route path="/" exact={true} component={Home()} />
 					<Route path="/home" exact={true} component={Home()} />
+					<Route path="/code" exact={true} component={Code()} />
 				</IonRouterOutlet>
 			</IonReactRouter>
 		</IonApp>
