36bea930b7cb659175f1df5f2f6563952445a65c
YaronKoresh <aharonkoresh1@gmail.com>
2021-09-03 21:32:11 +0300

    Add files via upload

---
+++ Mobile/index.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+import App, {Giselle, c, Home, Code} from './App';
+
+render(<Giselle/>,<c/>,<Home/>,<Code/>,<App/>,window.document.getElementById('root'));
