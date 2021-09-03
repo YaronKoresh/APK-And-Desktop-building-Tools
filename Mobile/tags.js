c2654b6e4a3d17d010c127637af21294eeedd244
YaronKoresh <aharonkoresh1@gmail.com>
2021-09-04 01:49:31 +0300

    Delete Mobile directory

--- Mobile/tags.js
+++
@@ -1,2 +0,0 @@
-var seps = window.document.getElementsByTagName( "sep" ) ; for ( var i = 0 ; i < seps.length ; i += 1 ) { seps[i].innerHTML = '&zwnj;</sep>' }
-var spans = window.document.getElementsByTagName( "span" ) ; for ( var j = 0 ; j < spans.length ; j += 1 ) { spans[j].insertAdjacentHTML ( "afterbegin" , '&zwnj;' ) ; spans[j].innerHTML += '&zwnj;' }
